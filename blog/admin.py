09a0ef92a793565e7615feff5c4d88997029427f
Subhash Rowtu <subhash@krify.com>
2026-01-03 08:06:38 +0530

    1.changes in blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category,Blog
 
 # Register your models here.
 
+class BlogModel(admin.ModelAdmin):
+    prepopulated_fields={
+        'slug':('title',)
+    }
+    
+    list_display=('title','category','author','is_featured','created_at')
+    search_fields=('id','title')
+    list_editable=('is_featured',)
+    
+    
+    
+
 admin.site.register(Category)
+admin.site.register(Blog,BlogModel)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
@@ -14,20 +14,29 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
     
-    
+STATUS_CHOICES=(
+    (0,"Draft"),
+    (1,"Published")
+)
     
 class Blog(models.Model):
     title= models.CharField(max_length=100,blank=False)
     slug= models.SlugField(max_length=150,unique=True,blank=True)
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
     
-    author=models.ForeignKey
-    #featured_img=
-    #ahort_description=
-    #blog_body=
-    #status=
-   # is_featured=
-   # created_at=
-   # updated_at=
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    featured_img=models.ImageField(upload_to='upload/%Y/%m/%d')
+    short_description=models.TextField(max_length=500)
+    blog_body=models.TextField(max_length=2000)
+    status=models.IntegerField(choices=STATUS_CHOICES,default=0)
+    is_featured=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+   
+   
+   
     
     
