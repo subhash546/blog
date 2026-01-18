4e3a3043baa8856d6e561f6057f64b299b4bfaa6
Subhash Rowtu <subhash@krify.com>
2026-01-18 19:22:52 +0530

    1.changes in setting.py

--- blogmain/settings.py
+++ blogmain/settings.py
@@ -128,10 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT= BASE_DIR/'static'
-STATICFILES_DIRS=[
-    BASE_DIR /'blogmain/mainstatic'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'blogmain/mainstatic',
 ]
 
 
