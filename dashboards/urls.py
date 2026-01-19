ed8bdfed9f10b9ad95d4b5f5daf2da03519ed1f4
Subhash Rowtu <subhash@krify.com>
2026-01-19 08:13:08 +0530

    1.changes in edit and delete category

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import dashboard,category,categoryadd
+from .views import dashboard,category,categoryadd,categoryEdit,categorydelete
 
 urlpatterns=[
     path('',dashboard,name="dashboard"),
     path('/category',category,name="category"),
-    path('/category/add',categoryadd,name="Addcategory")
+    path('/category/add',categoryadd,name="Addcategory"),
+    path('/category/edit/<int:pk>/',categoryEdit,name="Editcategory"),
+    path('/category/delete/<int:pk>/',categorydelete,name="deletecategory"),
+    
 ]
--- dashboards/views.py
+++ dashboards/views.py
@@ -4,6 +4,7 @@ from blog.models import Blog,Category
 from .forms import CategoryForm
 
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -36,3 +37,26 @@ def categoryadd(request):
         "form":form
     }
     return render(request,"dashboard/addcategory.html",context)
+
+
+def categoryEdit(request,pk):
+    category=get_object_or_404(Category,pk=pk)
+    if request.method == "POST":
+        form=CategoryForm( request.POST,instance=category)
+        if form.is_valid():
+            form.save()
+        return redirect("category")
+    else:
+        
+     form= CategoryForm(instance=category)
+    context={
+        "form":form,
+        "category":category
+    }
+    
+    return render(request,"dashboard/editcategory.html",context)
+
+def categorydelete(request,pk):
+    category=get_object_or_404(Category,pk=pk)
+    category.delete()
+    return redirect("category")
